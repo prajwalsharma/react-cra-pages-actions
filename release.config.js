ab3e18a554bf2430de625f00f109916a3c5d6706
Prajwal Sharma <prjwl4@gmail.com>
2023-04-24 20:39:20 +0530

    fix: eslint & semantic release

--- release.config.js
+++ release.config.js
@@ -1,23 +1,21 @@
 /* eslint-disable no-template-curly-in-string */
-export default  {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-          message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
-        },
-      ],
-      [
-        '@semantic-release/github',
-        {
-          assets: 'dist/**',
-        },
-      ],
-    ],
-  };
+export const branches = ['main'];
+export const plugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/changelog',
+  [
+    '@semantic-release/git',
+    {
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+    },
+  ],
+  [
+    '@semantic-release/github',
+    {
+      assets: 'dist/**',
+    },
+  ],
+];
   
