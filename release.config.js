8d0c8715e3ffa0d360edcb66e7152962c56305fb
Prajwal Sharma <prjwl4@gmail.com>
2023-04-24 13:17:26 +0530

    feat: use semantic release

---
+++ release.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+          message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        },
+      ],
+      [
+        '@semantic-release/github',
+        {
+          assets: 'dist/**',
+        },
+      ],
+    ],
+  };
+  
